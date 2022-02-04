62d8cfa6504fbf83b2b6d028a58a98fb5ab3c5f1
Mohamed CHENTOUF <chentoufmed@gmail.com>
2022-02-04 19:57:40 +0100

    second assignement Angular

--- app.js
+++ app.js
@@ -1,33 +1,84 @@
-(function() {
-  'use strict';
+(function () {
+	'use strict';
 
-  angular.module('LunchCheck', [])
-      .controller('LunchCheckController', LunchCheckController);
+	angular.module('ShoppingListCheckOff', [])
+	.controller('ToBuyShoppingController', ToBuyShoppingController)
+	.controller('AlreadyBoughtShoppingController', AlreadyBoughtShoppingController)
+	.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
-  LunchCheckController.$inject = ['$scope'];
+	ToBuyShoppingController.$inject = ['ShoppingListCheckOffService'];
+	function ToBuyShoppingController (ShoppingListCheckOffService) {
+		var toBuy = this;
+		toBuy.items = ShoppingListCheckOffService.getToBuyItems();
+		toBuy.moveToBought = function (itemIndex) {
+			ShoppingListCheckOffService.moveToBought(itemIndex);
+		};
+	}
 
-  function LunchCheckController($scope) {
-      $scope.dishes = '';
-      $scope.message = '';
-      $scope.checked = false;
+	AlreadyBoughtShoppingController.$inject = ['ShoppingListCheckOffService'];
+	function AlreadyBoughtShoppingController (ShoppingListCheckOffService) {
+		var bought = this;
+		bought.items = ShoppingListCheckOffService.getBoughtItems();
+	}
 
-      $scope.checkLunch = function() {
-          if ($scope.dishes.trim().length === 0) {
-              $scope.empty = true;
-          } else {
-              $scope.checked = true;
-              $scope.empty = false;
-              var arrayDishes = $scope.dishes.split(',');
-              var arrayDishesWithoutEmptys = arrayDishes.filter(function(v) {
-                  return v.trim() !== '';
-              });
+	function ShoppingListCheckOffService () {
+		var service = this;
 
-              if (arrayDishesWithoutEmptys.length <= 3) {
-                  $scope.message = 'Enjoy!';
-              } else {
-                  $scope.message = 'Too much!';
-              }
-          }
-      };
-  }
+		var toBuyItems = [{
+			name : "Cookies",
+			quantity: "10 bags"
+		},
+		{
+			name : "Milk",
+			quantity:"5 bottles"
+		},
+		{
+			name : "Donuts",
+			quantity : "4 bags"
+		},
+		{
+			name : "Chocolate",
+			quantity : "10 bags"
+		},
+		{
+			name : "Peanut Butter",
+			quantity : "4 bottles"
+		},
+		{
+			name : "Pepto Bismol",
+			quantity : "2 bottles"
+		},
+		{
+			name : "Pepto Bismol (Chocolate flavor)",
+			quantity : "4 bottles"
+		},
+		{
+			name : "Pepto Bismol (Cookie flavor)",
+			quantity : "2 bottles"
+		},
+		{
+			name : "Humberger",
+			quantity : "3 bags"
+		},
+		{
+			name : "Fruit juice",
+			quantity : "6 bottles"
+		}
+		];
+
+		var boughtItems = [];
+
+		service.getToBuyItems = function () {
+			return toBuyItems;
+		};
+
+		service.getBoughtItems = function () {
+			return boughtItems;
+		};
+
+		service.moveToBought = function (itemIndex) {
+			boughtItems.push(toBuyItems[itemIndex]);
+			toBuyItems.splice(itemIndex,1);
+		};
+	}
 })();
